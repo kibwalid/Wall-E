e9a082677c1e864fcc74e42c557bd7eaf219ea72
Khalid Ibnul <kibwalid@gmail.com>
2023-12-28 18:47:45 +0600

    Added speech recognition

---
+++ audio/speech_to_text.py
@@ -0,0 +1,22 @@
+import json
+
+import speech_recognition as sr
+
+
+def convert_speech_to_text():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something...")
+        recognizer.adjust_for_ambient_noise(source)
+        try:
+            audio = recognizer.listen(source, timeout=5)
+            text = recognizer.recognize_vosk(audio)
+            result = json.loads(text)
+            print(result['text'])
+            return text
+        except sr.UnknownValueError:
+            print("Could not understand audio.")
+            return None
+        except sr.RequestError as e:
+            print("Error with the speech recognition service; {0}".format(e))
+            return None
